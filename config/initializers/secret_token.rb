058b11cfb2a10144e62ee63b7f91d3b9ca93ff93
James B <james@jbergy.me>
2013-10-01 20:06:31 -0600

    Empty rails app with slim-rails installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bestforum::Application.config.secret_key_base = 'e9ee98194205122a368fbd626a013176aacfb53837e588cfafb1c14f08f14339d92613658bd1cd36c6bab7ad6a043844bfb90b54d000d31b8be1c30d34915a42'
